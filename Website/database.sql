b1bd0f6cfe885282fb3d10d747df5dc989f2159d
Daniel Blaine Martinez <57377837+dbmartinez@users.noreply.github.com>
2022-03-15 19:11:04 -0700

    Add files via upload

---
+++ Website/database.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE contact(
+	FirstName char(255) NOT NULL,
+  	LastName char (255) NOT NULL,
+  	Email varchar(50) NOT NULL,
+  	Subject varchar(255) NOT NULL,
+  	Message varchar(255) NOT NULL
+);
+
+CREATE TABLE shopping(
+
+
+);
+
