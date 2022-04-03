9fd180543506024219d22c43f4916014b8e958c8
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2022-04-03 13:22:42 -0700

    Update database.sql

--- Website/database.sql
+++ Website/database.sql
@@ -8,8 +8,13 @@ CREATE TABLE contact(
   	Message varchar(255) NOT NULL
 );
 
-CREATE TABLE shopping(
-
-
+CREATE TABLE user (
+    uid int(8) not null,
+    LastName varchar(255),
+    FirstName varchar(255),
+    password varchar(255),
+    Address varchar(255),
+    creditcardnum int(30),
+    contact_flag int(1)
 );
 
