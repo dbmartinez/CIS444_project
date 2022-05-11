56e49fcbc6bf4e7780ca6659abbc2c78e3fe933c
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2022-05-10 17:45:01 -0700

    Update shop_javascript.js

--- Website/shop_javascript.js
+++ Website/shop_javascript.js
@@ -35,8 +35,58 @@ function calculateTotal(){
 	return false;
 }
 
-function addItem(){
-		alert("item(s) added/removed in cart");
+function addCz(){
+  
+  var qtyCz = document.getElementById("cz").value;
+  var priceCz = 8.89;
+  var totCz = (priceCz * qtyCz);
+  
+	alert(qtyCz + " coozie(s) in cart\n" + "Price: $" + totCz );
+  
+	return false;
+}
+
+function addMug(){
+  
+  var qtyMug = document.getElementById("mug").value;
+  var priceMug = 12.69;
+  var totMug = (priceMug*qtyMug);
+  
+	alert(qtyMug + " mug(s) in cart\n" + "Price: $" +totMug );
+  
+	return false;
+}
+
+function addHd(){
+  
+  var qtyHd = document.getElementById("hd").value;
+  var priceHd = 35.49;
+  var totHd = (priceHd*qtyHd);
+  
+	alert(qtyHd + " hoodie(s) in cart\n" + "Price: $" +totHd );
+  
+	return false;
+}
+
+function addTs(){
+  
+  var qtyTs = document.getElementById("ts").value;
+  var priceTs = 25.49;
+  var totTs = (priceTs*qtyTs);
+  
+	alert(qtyTs + " t-shirt(s) in cart\n" + "Price: $" +totTs );
+  
+	return false;
+}
+
+function addHat(){
+  
+  var qtyHat = document.getElementById("hat").value;
+  var priceHat = 25.49;
+  var totHat = (priceHat*qtyHat);
+  
+	alert(qtyHat + " hat(s) in cart \n" + "Price: $" +totHat );
+  
 	return false;
 }
 
