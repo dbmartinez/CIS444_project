b398b3d4d47d4df4273efdf9e305bf97d47610ff
tabar010 <71281530+tabar010@users.noreply.github.com>
2022-05-10 19:36:00 -0700

    added totaling func and globals

--- Website/beer_dashboard.js
+++ Website/beer_dashboard.js
@@ -1,6 +1,25 @@
 var canPrice = 4.50;
 var sixPackPrice = 24.75;
 var taxRate = 1.0775;
+var beerTotal = 0;
+var qtyFrankCan = 0;
+var qtyFrankSix = 0;
+var qtyKobyCan = 0;
+var qtyKobySix = 0;
+var qtyLuffyCan = 0;
+var qtyLuffySix = 0;
+var qtyNamiCan = 0;
+var qtyNamiSix = 0;
+var qtyRobinCan = 0;
+var qtyRobinSix = 0;
+var qtySanjiCan = 0;
+var qtySanjiSix = 0;
+var qtyTonyCan = 0;
+var qtyTonySix = 0;
+var qtyUsoppCan = 0;
+var qtyUsoppSix = 0;
+var qtyZoroCan = 0;
+var qtyZoroSix = 0;
 
 function calcFrank() {
 	var qty = document.getElementById("FP").value;
@@ -9,14 +28,19 @@ function calcFrank() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyFrankCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyFrankSix += (qty * 1)
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("FP").value = 0;
 }
 
 function calcKoby() {
@@ -26,14 +50,19 @@ function calcKoby() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyKobyCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyKobySix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("KT").value = 0;
 }
 
 function calcLuffy() {
@@ -43,14 +72,19 @@ function calcLuffy() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyLuffyCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyLuffySix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("LPA").value = 0;
 }
 
 function calcNami() {
@@ -60,14 +94,19 @@ function calcNami() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyNamiCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyNamiSix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("NSS").value = 0;
 }
 
 function calcRobin() {
@@ -77,14 +116,19 @@ function calcRobin() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyRobinCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyRobinSix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("RML").value = 0;
 }
 
 function calcSanji() {
@@ -94,14 +138,19 @@ function calcSanji() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtySanjiCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtySanjiSix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("SPL").value = 0;
 }
 
 function calcTony() {
@@ -111,14 +160,19 @@ function calcTony() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyTonyCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyTonySix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("TC").value = 0;
 }
 
 function calcUssop() {
@@ -128,14 +182,19 @@ function calcUssop() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyUsoppCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyUsoppSix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("US").value = 0;
 }
 
 function calcZoro() {
@@ -145,12 +204,87 @@ function calcZoro() {
 	
 	if (radios[0].checked) {
 		var total = qty * canPrice * taxRate;
+		beerTotal += total;
+		qtyZoroCan += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " can(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
 	else {
 		var total = qty * sixPackPrice * taxRate;
+		beerTotal += total;
+		qtyZoroSix += (qty * 1);
 		total = total.toFixed(2);
 		alert("Added " + qty + " six pack(s) of " + beerName + " to cart (amount added $" + total + ").");
 	}
+	document.getElementById("ZD").value = 0;
+}
+
+function calcTotal() {
+	var beerTotalStr = beerTotal.toFixed(2);
+	var orderSummary = "";
+	
+	if (qtyFrankCan > 0) {
+		orderSummary += "Can(s) of Frank Porter: " + qtyFrankCan + "\n";
+	}
+	if (qtyFrankSix > 0) {
+		orderSummary += "Six pack(s) of Frank Porter: " + qtyFrankSix + "\n";
+	}
+
+	if (qtyKobyCan > 0) {
+		orderSummary += "Can(s) of Koby Tripel: " + qtyKobyCan + "\n";
+	}
+	if (qtyKobySix > 0) {
+		orderSummary += "Six pack(s) of Koby Tripel: " + qtyKobySix + "\n";
+	}
+
+	if (qtyLuffyCan > 0) {
+		orderSummary += "Can(s) of Luffy Pale Ale: " + qtyLuffyCan + "\n";
+	}
+	if (qtyLuffySix > 0) {
+		orderSummary += "Six pack(s) of Luffy Pale Ale: " + qtyLuffySix + "\n";
+	}
+
+	if (qtyNamiCan > 0) {
+		orderSummary += "Can(s) of Nami S&S Pilsner: " + qtyNamiCan + "\n";
+	}
+	if (qtyNamiSix > 0) {
+		orderSummary += "Six pack(s) of Nami S&S Pilsner: " + qtyNamiSix + "\n";
+	}
+
+	if (qtyRobinCan > 0) {
+		orderSummary += "Can(s) of Robin Mexican Lager: " + qtyRobinCan + "\n";
+	}
+	if (qtyRobinSix > 0) {
+		orderSummary += "Six pack(s) of Robin Mexican Lager: " + qtyRobinSix + "\n";
+	}
+
+	if (qtySanjiCan > 0) {
+		orderSummary += "Can(s) of Sanji Pale Lager: " + qtySanjiCan + "\n";
+	}
+	if (qtySanjiSix > 0) {
+		orderSummary += "Six pack(s) of Sanji Pale Lager: " + qtySanjiSix + "\n";
+	}
+
+	if (qtyTonyCan > 0) {
+		orderSummary += "Can(s) of Tony Chopper IPA: " + qtyTonyCan + "\n";
+	}
+	if (qtyTonySix > 0) {
+		orderSummary += "Six pack(s) of Tony Chopper IPA: " + qtyTonySix + "\n";
+	}
+
+	if (qtyUsoppCan > 0) {
+		orderSummary += "Can(s) of Usopp Stout: " + qtyUsoppCan + "\n";
+	}
+	if (qtyUsoppSix > 0) {
+		orderSummary += "Six pack(s) of Usopp Stout: " + qtyUsoppSix + "\n";
+	}
+
+	if (qtyZoroCan > 0) {
+		orderSummary += "Can(s) of Zoro Double IPA: " + qtyZoroCan + "\n";
+	}
+	if (qtyZoroSix > 0) {
+		orderSummary += "Six pack(s) of Zoro Double IPA: " + qtyZoroSix + "\n";
+	}
+	
+	alert("Your order has been submitted!\n\n" + "Items ordered:\n" + orderSummary + "\nYour total is: $" + beerTotalStr + "\n\nThank you!");
 }
